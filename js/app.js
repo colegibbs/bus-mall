2a0b69bce55e11d512a2eb8f24f2a321c6ac47ec
Cole Gibbs <colegibbs0@gmail.com>
2022-02-21 14:21:58 -0800

    instantiated all products

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 //Global variables
   //DOM windows
 let container = document.getElementById('container');
-let imgOne = doucment.getElementById('img-one');
+let imgOne = document.getElementById('img-one');
 let imgTwo = document.getElementById('img-two');
 let imgThree = document.getElementById('img-three');
 let resultBtn = document.getElementById('result-btn');
@@ -16,7 +16,28 @@ function Product(name, fileType = 'jpeg') {
   this.src = `${name}.${fileType}`;
   this.timesShown = 0;
   this.timesClicked = 0;
+  allProducts.push(this);
 }
+// instantiate objects using Product constructor
+new Product('bag');
+new Product('banana');
+new Product('bathroom');
+new Product('boots');
+new Product('breakfast');
+new Product('bubblegum');
+new Product('chair');
+new Product('cthulhu');
+new Product('dog-duck');
+new Product('dragon');
+new Product('pen');
+new Product('pet-sweep');
+new Product('scissors');
+new Product('shark');
+new Product('sweep', 'png');
+new Product('tauntaun');
+new Product('unicorn');
+new Product('water-can');
+new Product('wine-glass');
 //function for rendering products
 //event handler for click
 //event listener for click
