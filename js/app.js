3c4ecca2037897f3a7c8586274a69b23203129d6
Cole Gibbs <colegibbs0@gmail.com>
2022-02-21 14:10:02 -0800

    pseudo and dom windows

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//Global variables
+  //DOM windows
+let container = document.getElementById('container');
+let imgOne = doucment.getElementById('img-one');
+let imgTwo = document.getElementById('img-two');
+let imgThree = document.getElementById('img-three');
+let resultBtn = document.getElementById('result-btn');
+let resultList = document.getElementById('result-list');
+  //all products array
+//Constructor - product name, img filepath, # of times the item has been shown, number of times the item has been clicked
+//function for rendering products
+//event handler for click
+//event listener for click
+//event handler for result button
+//event listener for resutl button
