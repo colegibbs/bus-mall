a196d5b189470b453716b71cfb63588266d30e0e
Cole Gibbs <colegibbs0@gmail.com>
2022-02-21 14:10:57 -0800

    all products array

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ let imgThree = document.getElementById('img-three');
 let resultBtn = document.getElementById('result-btn');
 let resultList = document.getElementById('result-list');
   //all products array
+let allProducts = [];
 //Constructor - product name, img filepath, # of times the item has been shown, number of times the item has been clicked
 //function for rendering products
 //event handler for click
